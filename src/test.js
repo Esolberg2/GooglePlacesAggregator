05b04367690c08798f548059d0e8f348c6b4d518
Eric Solberg <28678435+Esolberg2@users.noreply.github.com>
2022-06-28 10:48:55 -0400

    modified promises and added logic to refresh google maps javascript api library to reflect api key changes
    Former-commit-id: b8955255777a2c81ad694d7cbbf1b05061eca9eb

--- src/test.js
+++ src/test.js
@@ -1,53 +1,11 @@
-
-const data = require('../test.json');
-var fs = require('fs');
-var CryptoJS = require("crypto-js");
-
-// let searchedString = JSON.stringify(data["1"]["searched"])
-let searchedString = Buffer.from(data["1"]["searched"])
-console.log(searchedString)
-
-function encryptData(data) {
-  var key = "bookbookbook";
-  var encrypted = CryptoJS.HmacSHA1(data, key);
-  return CryptoJS.enc.Base64.stringify(encrypted)
+function loggerDecorator (logger) {
+    return function (message) {
+        logger.call(this, message)
+        console.log("message logged at:", new Date().toLocaleString())
+    }
 }
 
-console.log(encryptData(searchedString))
-// function encrypt(data) {
-//   var key = "bookbookbook";
-  // var encrypted = CryptoJS.HmacSHA1(data, key);
-
-// console.log(encrypt(searchedString))
-
-  // fs.writeFile("jsString.txt", searchedString, function(err) {
-  //   if (err) {
-  //     console.log(err);
-  //     }
-  //   });
-
-// console.log(data["1"]["searched"]);
-
-// let searched = data["1"]["searched"]
-//
-// function encrypt(data) {
-//   var CryptoJS = require("crypto-js");
-//   var key = "bookbookbook";
-//   var msg = JSON.stringify(data);
-//   console.log(type(msg)
-//   var encrypted = CryptoJS.HmacSHA1(msg, key);
-//   return encrypted
-
-//
-//   var fs = require('fs');
-//   fs.writeFile("jsJson.json", msg, function(err) {
-//       if (err) {
-//           console.log(err);
-//       }
-//   });
-//
-//   console.log("encrypted in Base64: " + CryptoJS.enc.Base64.stringify(encrypted));
-// }
-//
-// encrypt(searched)
-// //53EMUiX5fQKSfCBXcDs7xHzeQYU=
+@loggerDecorator
+function say(message) {
+  console.log(message)
+}
