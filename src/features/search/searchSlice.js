6813150e9cd5b36798a0bcec7cc50dd2f3b957d5
Eric Solberg <28678435+Esolberg2@users.noreply.github.com>
2022-11-17 00:17:55 -0500

    added nearbySearch to googlePlacesApiManager along with state hooks.
    Former-commit-id: 5f47b400a49456af05e0f0341f5ff39b892a01e0

--- src/features/search/searchSlice.js
+++ src/features/search/searchSlice.js
@@ -1,43 +1,68 @@
-import { createSlice } from '@reduxjs/toolkit'
-import {initializeSearch} from './buildSearchSlice'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { googlePlacesApiManager } from '../../googleAPI/googlePlacesApiManager'
+import axios from 'axios'
+
 const initialState = {
+  loading: false,
+  data: [],
+  error: '',
   searchCallType: "",
   searchReady: false,
   searchComplete: false,
   bulkSearchCount: 0,
-  data: {
-    searchedData: [],
-    unsearchedData: [],
-    nextCenter: null,
-    searchCentroid: null,
-    searchID: null,
-  }
 }
 
+
+// ============= Thunks ==================
+export const initializeSearch = createAsyncThunk('search/initializeSearch',(a, b) => {
+  const polygons = b.getState().map.polygons
+  const searchResolution = b.getState().settingsPanel.searchResolution
+  return axios
+    .post(`/api/searchSession`, {
+    "searchRegions": polygons,
+    "searchID": null,
+    "coordinateResolution": searchResolution
+    })
+    .then((response) => response.data)
+})
+
+export const nearbySearch = createAsyncThunk('search/nearbySearch',(a, b) => {
+  let rawGoogleData = googlePlacesApiManager()
+  return axios
+    .post(`/api/searchSession`, {
+    "searchRegions": null,
+    "searchID": null,
+    "coordinateResolution": null
+    })
+    .then((response) => response.data)
+})
+
+// ============ Reducers ====================
 export const searchSlice = createSlice({
-  name: 'search',
+  name: 'searchSlice',
   initialState,
   extraReducers: (builder) => {
-
-    builder.addCase(initializeSearch.fulfilled, (state, action) => {
-      state.data.searchedData = action.payload.searchedCoords
-      state.data.unsearchedData = action.payload.unsearchedCoords
-      state.data.nextCenter = action.payload.furthestNearest
-      state.data.searchCentroid = action.payload.furthestNearest
-      state.data.searchID = action.payload.searchID.lastRowID
+    builder.addCase(initializeSearch.pending, (state) => {
+      state.loading = true
     })
-
+    builder.addCase(initializeSearch.fulfilled, (state, action) => {
+      state.loading = false
+      state.data = action.payload
+      state.error = ''
+    })
+    builder.addCase(initializeSearch.rejected, (state, action) => {
+      state.loading = false
+      state.data = []
+      state.error = action.error.message
+    })
   },
   reducers: {
     addSearchCallType: (state, action) => {state.searchCallType = action.payload},
     setSearchReady: (state, action) => {state.searchReady = action.payload},
     setSearchComplete: (state, action) => {state.searchComplete = action.payload},
-    setBulkSearchCount: (state, action) => {state.bulkSearchCount = action.payload},
-    setData: (state, action) => {state.data = action.payload},
+    setBulkSearchCount: (state, action) => {state.bulkSearchCount = action.payload}
   },
-
 })
 
 export const searchActions = searchSlice.actions
-
 export default searchSlice.reducer
--- src/store.js
+++ src/store.js
@@ -1,14 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import mapReducer from './features/map/mapSlice'
 import settingsPanelReducer from './features/settingsPanel/settingsPanelSlice'
-import buildSearchReducer from './features/search/buildSearchSlice'
 import searchReducer from './features/search/searchSlice'
 
 export const store = configureStore({
   reducer: {
     map: mapReducer,
     settingsPanel: settingsPanelReducer,
-    buildSearch: buildSearchReducer,
     search: searchReducer,
   },
 })
