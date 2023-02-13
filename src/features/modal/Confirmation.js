36b5f694da9da253e0065eda87a9bd2dd6da42ff
Eric Solberg <28678435+Esolberg2@users.noreply.github.com>
2023-02-12 21:48:48 -0500

    Moved react-modal to centralized modal component, and converted Alert and Confirmation components to become containers.
    Former-commit-id: 500c1571586c91433c3574d39941885b22a69624

--- src/features/modal/Confirmation.js
+++ src/features/modal/Confirmation.js
@@ -1,49 +1,28 @@
 import React, { useEffect, useRef, useState, useCallback } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { modalFunctionStore, setVisible, callbackDict } from './modalSlice'
-import Modal from 'react-modal';
-Modal.setAppElement('body')
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
-  overlay: {
-    zIndex: 9999,
-  }
-};
-
-export const ConfirmationModal = React.memo(props => {
-  const visible = useSelector((state) => state.modal.visible)
-  const message = useSelector((state) => state.modal.message)
-  const resolve = modalFunctionStore.resolve
-  const reject = modalFunctionStore.reject
-
+export function ConfirmationModal(props) {
+  const {
+    confirmCallback,
+    rejectCallback,
+    message,
+    visible
+  } = props
 
   function onConfirm() {
-    resolve()
+    confirmCallback()
+    return true
   }
 
   function onDeny() {
-    reject()
+    rejectCallback()
+    return false
   }
 
   return (
     <div>
-      <Modal
-        isOpen={visible}
-        style={customStyles}
-        contentLabel="Example Modal"
-      >
-        <button onClick={onConfirm}>confirm</button>
-        <button onClick={onDeny}>deny</button>
-        <div>{message}</div>
-      </Modal>
+      <div>{message}</div>
+      <button onClick={onConfirm}>confirm</button>
+      <button onClick={onDeny}>deny</button>
     </div>
   );
-})
+}
