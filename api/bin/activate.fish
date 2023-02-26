056d008657cadd77744a25af478ace615733e97d
Eric Solberg <28678435+Esolberg2@users.noreply.github.com>
2023-02-25 21:50:50 -0500

    bfg run

--- api/bin/activate.fish
+++ api/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/eric/Documents/_ReactJS/aggregator_clone/docker_clone/aggregator_clone_docker/api"
+set -gx VIRTUAL_ENV "/Users/eric/Documents/_ReactJS/aggregator_clone/docker_clone/test/testClone/api"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
