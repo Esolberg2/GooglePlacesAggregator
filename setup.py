bd574c251bbf3ec7c1b52775d20d332fd2fde6b6
Eric Solberg <28678435+Esolberg2@users.noreply.github.com>
2023-02-26 23:17:56 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -11,10 +11,6 @@ react_env_exists = os.path.isfile('{}/.env'.format(dir_path))
 print(env_exists)
 print(flask_env_exists)
 print(react_env_exists)
-
-# if env_exists:
-#     print("Existing directory named env found in project root. Please delete this directory before rerunning setup.py.")
-#     sys.exit()
    
 if flask_env_exists:
     print("existing flask env file found")
@@ -37,43 +33,54 @@ else:
     shutil.copy('{}/requirements.txt'.format(dir_path), '{}/env'.format(dir_path))
 
     install_requirements_cmd = 'cd env; source bin/activate; pip install -r requirements.txt'
-    # os.chdir('{}/env'.format(dir_path))
-    # os.system('which python')
-    # os.system('cd env; source bin/activate; which python')
-    # os.system('pwd')
     os.system(install_requirements_cmd)
-    # os.chdir(dir_path)
-    # os.system('pwd')
 
     build_flask_dot_env_file = open('{}/api/.env'.format(dir_path), 'x+')
     if build_flask_dot_env_file:
         build_flask_dot_env_file.write("SQLALCHEMY_DATABASE_URI='sqlite:///searches.sqlite3'\n")
-        flask_secret_key = input("secret key for SQLALCHEMY: can be anything you would like  ")
+        print("")
+        print("")
+        print("SET SQLALCHEMY SECRET KEY")
+        flask_secret_key = input("secret key for SQLALCHEMY: can be anything you would like:  ")
+        while flask_secret_key == '':
+            print("please enter a secret key. It really can be anything. Just type something, anything... we believe in you")
+            flask_secret_key = input("secret key for SQLALCHEMY: can be anything you would like:  ")
         build_flask_dot_env_file.write("SECRET_KEY='{}'".format(flask_secret_key))
         build_flask_dot_env_file.close()
 
     build_react_dot_env_file = open('{}/.env'.format(dir_path), 'x+')
     if build_react_dot_env_file:
-        mapbox_token = input("Enter your mapbox token.  A missingor incorrect token will cause the UI to fail to render. To obtain a key, create a free account with Mapbox.  ")
+        print("")
+        print("")
+        print("SET MAPBOX TOKEN")
+        mapbox_token = input("Enter your mapbox token.  A missingor incorrect token will cause the UI to fail to render. To obtain a key, create a free account with Mapbox:  ")
+        while mapbox_token == '':
+            print("")
+            mapbox_token = input("Enter your mapbox token.  A missingor incorrect token will cause the UI to fail to render. To obtain a key, create a free account with Mapbox:  ")
         build_react_dot_env_file.write("REACT_APP_MAPBOX_ACCESS_TOKEN={}".format(mapbox_token))
         build_react_dot_env_file.close()
 
-    # shutil.copy('{}/wsgi.py'.format(dir_path), '{}/env/wsgi.py'.format(dir_path))
-
-    package_manager = input("Will you be using Yarn or NPM for this project? Please enter Yarn or NPM to continue  ")
+    print("")
+    print("")
+    print("SET PACKAGE MANAGER")
+    package_manager = input("Will you be using Yarn or NPM for this project? Please enter Yarn or NPM to continue:  ")
     while package_manager.lower() not in ('yarn', 'npm'):
-        package_manager = input("Will you be using Yarn or NPM for this project? Please enter Yarn or NPM to continue  ")
+        print("")
+        package_manager = input("Will you be using Yarn or NPM for this project? Please enter Yarn or NPM to continue:  ")
 
-    continue_install = input("Are you ready to install dependencies using {}? (y/n)  ".format(package_manager))
+    continue_install = input("Are you ready to install dependencies using {}? (y/n):  ".format(package_manager))
     while continue_install.lower() not in ('y', 'n'):
-            continue_install = input("Are you ready to install dependencies using {}? (y/n)  ".format(package_manager))
+            print("")
+            continue_install = input("Are you ready to install dependencies using {}? (y/n):  ".format(package_manager))
 
     if continue_install.lower() == 'y':
         if package_manager.lower() == 'yarn':
             os.system('yarn install')
         if package_manager.lower() == 'npm':
             os.system('npm install')
-
+        print("")
+        print("SETUP COMPLETE")
+        print("USAGE NOTES")
         msg = """
         Use the following terminal commands from the project root directory to run the application:
         npm start     to run the react UI.
@@ -83,6 +90,9 @@ else:
         print(msg)
 
     else:
+        print("")
+        print("")
+        print("USAGE NOTES")
         print('When you are ready, install use your preferred package manager to install react project dependencies listed in package.json')
         msg = """
         After installing react dependencies use the following terminal commands from the project root directory to run the application:
